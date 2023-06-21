3fcd22ba634c23ec11f4ed2cc1830df26493836a
sein Lee <lsi6601@gmail.com>
2023-06-21 14:19:53 +0900

    Revert "login"

--- src/main/java/service/MainService.java
+++ src/main/java/service/MainService.java
@@ -1,9 +1,7 @@
 package service;
 
 import dao.MainDao;
-import dao.UserDao;
 import dto.MovieDto;
-import dto.UserDto;
 
 import java.util.List;
 
@@ -23,15 +21,6 @@ public class MainService {
         List<MovieDto> getGenreList = MainDao.getRepository().getGenre(genre);
         return getGenreList;
     }
-    public UserDto loginById(String user_id, String user_pwd){
-        UserDto loginIdlUser = UserDao.getUserRepository().loginById(user_id, user_pwd);
-        return loginIdlUser;
-    }
-    public UserDto loginByEmail(String user_email, String user_pwd){
-        UserDto loginEmailUser = UserDao.getUserRepository().loginById(user_email, user_pwd);
-        return loginEmailUser;
-    }
-
 
     public MovieDto findMovieBySeq(int seq) {
         return MainDao.getRepository().findMovieBySeq(seq);
